05aedf5596e85bdd1fdb96b6606305875060eb4f
Angelot Andriamialy <angelot.andriamialy@gmail.com>
2021-06-07 21:17:59 +0300

    header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.scss';
 import HomePage from './pages/Homepage/Homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}/>  
         <Route exact path="/shop" component={Shop}/>
