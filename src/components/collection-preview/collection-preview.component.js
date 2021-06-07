eb12311b142d56ac9b0cc3942b07eb3bfbdea5f3
Angelot Andriamialy <angelot.andriamialy@gmail.com>
2021-06-07 20:46:12 +0300

    collections

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items   }) => {
     return (
         <div className="collection-preview">
             <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
-                {
-                    items.map((item) =>(
-                        <div key={item.id}>{item.name}</div>
+                {items
+                    .filter((item, index) => index < 4) //liste 4 max
+                    .map(({id, ...otherItemProps}) =>(
+                        <CollectionItem key={id} {...otherItemProps} />
                     ))
                 }
             </div>
